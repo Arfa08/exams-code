ad263380f2941bfb7d8b61c44f1d6f1e80f66f4b
Arfa08 <60848185+Arfa08@users.noreply.github.com>
2020-02-10 22:15:14 -0800

    Update SumOfArray.java

--- Examjava/src/array/SumOfArray.java
+++ Examjava/src/array/SumOfArray.java
@@ -1,5 +1,14 @@
 package array;
 
 public class SumOfArray {
-
+  public class SumOfArray {
+		 public static void main(String args[]){
+		      int[] array = {10, 240, 30, 88, 50, 55};
+		      int sum = 0;
+		      for( int num : array) {
+		          sum = sum+num;
+		      }
+		      System.out.println("Sum of array elements is:"+sum);
+		   }
 }
+
